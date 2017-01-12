c37bf622660c3e1711761ab501d22bb3cf28d9d7
Bernard Tatin <bernard.tatin@outlook.fr>
2017-01-12 08:05:32 +0100

    A better test/byte-read.scm

--- test/byte-read.scm
+++ test/byte-read.scm
@@ -1,27 +1,33 @@
 ;; -*- coding: utf-8 -*-
 
 (import (scheme base) (scheme char) (scheme lazy)
-        (scheme inexact) (scheme complex)
-        (scheme time)
-        (scheme file) (scheme read) (scheme write)
-        (scheme eval) (scheme process-context) (scheme case-lambda)
-        (only (scheme r5rs) scheme-report-environment null-environment)
-        (chibi test)  ; or (srfi 64)
-        )
+		(scheme inexact) (scheme complex)
+		(scheme time)
+		(scheme file) (scheme read) (scheme write)
+		(scheme eval) (scheme process-context) (scheme case-lambda)
+		(only (scheme r5rs) scheme-report-environment null-environment)
+		(chibi test)  ; or (srfi 64)
+		)
 
 
-(test-begin "R7RS")
 (test-begin "read byte file")
 
+(define-syntax rb-test
+  (syntax-rules ()
+				((rb-test expected-len bytes-to-read)
+				 (test expected-len  ;; "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+					   (let ((handle (open-binary-input-file "input.txt")))
+						 (let ((buffer (read-bytevector bytes-to-read handle)))
+						   (close-input-port handle)
+						   (display "<") (display buffer) (display ">\n")
+						   (if (eof-object? buffer)
+							 buffer
+							 (bytevector-length buffer))))))))
 
-(test 52  ;; "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	  (let ((handle (open-binary-input-file "input.txt")))
-		(let ((buffer (read-bytevector 512 handle)))
-		  (close-input-port handle)
-          (display "<") (display buffer) (display ">\n")
-		  52)))
 
-
-(test-end)
+(rb-test 3 3)
+(rb-test 25 25)
+(rb-test 52 52)
+(rb-test 52 128)
 
 (test-end)
